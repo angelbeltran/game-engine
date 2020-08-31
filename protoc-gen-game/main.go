117cc9b2247fd9fe201a70113c250d731326ca97
Angel Beltran <abeltran@solutionreach.com>
2020-08-30 21:15:05 -0600

    refer to generate/dst instead of protos

--- protoc-gen-game/main.go
+++ protoc-gen-game/main.go
@@ -13,16 +13,6 @@ import (
 
 //go:generate go generate ./generate
 //go:generate protoc -I=./generate/dst/proto --go_out=$GOPATH/src action.proto effect.proto error.proto extensions.proto reference.proto type.proto values_and_functions.proto
-
-//go:generate cp ./generate/dst/proto/action.proto protos/action.proto
-//go:generate cp ./generate/dst/proto/effect.proto protos/effect.proto
-//go:generate cp ./generate/dst/proto/error.proto protos/error.proto
-//go:generate cp ./generate/dst/proto/extensions.proto protos/extensions.proto
-//go:generate cp ./generate/dst/proto/reference.proto protos/reference.proto
-//go:generate cp ./generate/dst/proto/type.proto protos/type.proto
-//go:generate cp ./generate/dst/proto/values_and_functions.proto protos/values_and_functions.proto
-//go:generate cp ./generate/dst/proto/bundle.proto protos/bundle.proto
-
 //go:generate go mod vendor
 
 // TODO: get rid of 'types' package once no longer needed.
